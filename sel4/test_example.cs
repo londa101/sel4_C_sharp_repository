3a8ca6c7690f8fbab9c9ec8740de17c26e473ca5
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-02-21 20:01:27 +0100

    project added
    Selenium sel-4 lection 2 task 1

---
+++ sel4/test_example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Sel4
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "https://wizzair.com/";
+            var a = driver.Title;
+            wait.Until(ExpectedConditions.TitleIs("Official Wizz Air website | Book direct for the cheapest prices"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
