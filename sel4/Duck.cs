89896278ab01d823ab382019cdc13a9486e8188f
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-03-07 23:50:27 +0100

    add class Duck -info about product Add test public void ProductShouldHaveCorrectDetails() - not completed

---
+++ sel4/Duck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Duck
+{
+    public string Name;
+    public string RegularPrice;
+    public string CampaignPrice;
+    public string RegularPriceAttributes;
+    public string CampaignPriceAttribute;
+
+    public Duck(string name, string regularPrice, string campaignPrice, string regularPriceAttributes, string campaignPriceAttribute)
+	{
+        Name = name;
+        RegularPrice = regularPrice;
+        CampaignPrice = campaignPrice;
+        RegularPriceAttributes = regularPriceAttributes;
+        CampaignPriceAttribute = campaignPriceAttribute;
+
+    }
+}
--- sel4/ShopTests.cs
+++ sel4/ShopTests.cs
@@ -27,10 +27,46 @@ namespace Sel4
                     Assert.IsTrue(IsStickerExistFor(duck), $"Product '{GetProductId(duck)}' for '{GetSectionName(section)}' hasn't any sticker");
                 }
             }
-            
+        
+        }
 
+        [Test]
+        public void ProductShouldHaveCorrectDetails()
+        {
 
-            
+            GoToShopPage();
+            var duckInfoFromMainPage = GetInfoFromMainPage("Campaign", 1);
+            SelectProduct("Campaign", 1);
+            var duckInfoFromDetails = GetInfoFromDetailsPage();
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(duckInfoFromMainPage.Name, duckInfoFromDetails.Name);
+                Assert.AreEqual(duckInfoFromMainPage.RegularPrice, duckInfoFromDetails.RegularPrice);
+                Assert.AreEqual(duckInfoFromMainPage.CampaignPrice, duckInfoFromDetails.CampaignPrice);
+            });
+            /*
+               в) обычная цена серая и зачёркнутая, а акционная цена красная и жирная (это надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
+               г) акционная цена крупнее, чем обычная (это надо проверить на каждой странице независимо)
+             */
+            var a = 1;
+
+        }
+
+        private Duck GetInfoFromDetailsPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Duck GetInfoFromMainPage(string list, int index)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        private void SelectProduct(string listName, int item)
+        {
+            throw new NotImplementedException();
         }
 
         private List<IWebElement> GetProductsFromSection(IWebElement section)
